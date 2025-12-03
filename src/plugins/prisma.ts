3b3416b0d68f288996ac4854cd6183a9ec8d5ca6
ARTHUR SANTOS TAVARES SOUTO <14255515999@senacrs.edu.br>
2025-12-03 14:11:22 -0300

    normalize: remove generated folder

--- src/plugins/prisma.ts
+++ src/plugins/prisma.ts
@@ -1,19 +1,17 @@
 import "dotenv/config"
-import {PrismaClient } from "../../generated/client"
+import { PrismaClient } from "../../generated/client"
 import { PrismaPg } from "@prisma/adapter-pg"
 import fp from "fastify-plugin"
 import { FastifyInstance } from "fastify"
 
-const pool = new PrismaPg({connectionString: process.env.DATABASE_URL})
-const prisma: PrismaClient = new PrismaClient({adapter: pool})
-
-
+const pool = new PrismaPg({ connectionString: process.env.DATABASE_URL })
+const prisma: PrismaClient = new PrismaClient({ adapter: pool })
 
 
 const prismaPlugin = fp(async (fastify: FastifyInstance) => {
   await prisma.$connect();
 
-  
+
   fastify.decorate("prisma", prisma as PrismaClient);
 
   fastify.addHook("onClose", async () => {
